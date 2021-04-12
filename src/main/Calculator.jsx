4d590c5839891515b5ec3ec9bfac34b3834528b9
IrleySilvestre <66978110+IrleySilvestre@users.noreply.github.com>
2021-04-12 19:26:29 -0300

    Quase terminada

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -3,7 +3,18 @@ import './Calculator.css'
 import {Button} from "../components/Button";
 import {Display} from "../components/Display";
 
+
+const initialState = {
+    displayValue : '0',
+    clear : false,
+    operation: null,
+    values : [0,0],
+    current : 0
+}
+
+
 export default class Calculator extends Component {
+    state = {...initialState}
     constructor(props) {
         super(props)
         this.clearDisplay = this.clearDisplay.bind(this)
@@ -12,21 +23,67 @@ export default class Calculator extends Component {
     }
 
     clearDisplay(){
-        console.log('limpar')
+        this.setState({...initialState})
+
     }
 
     setOperation(operation){
-        console.log(operation)
+
+        if (this.state.current === 0){
+            this.setState({
+                operation :operation,
+                current:1,
+                clear: true
+            })
+        }else {
+            const opSelect = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+            try{
+                values[0] =  eval(`${values[0]} ${currentOperation} ${values[1]} `)
+            }catch (e) {
+                values[0] = this.state.values[0]
+            }
+            values[1] = 0
+            this.setState({
+                displayValue: values[0],
+                operation: opSelect ? null : operation,
+                curret: opSelect ? 0 : 1,
+                clear: !opSelect,
+                values
+
+            })
+        }
     }
 
     addDigit(n){
-        console.log(n)
+        if (n === '.' && this.state.displayValue.includes('.')){
+            return
+        }
+
+        const clear = this.state.displayValue === '0' || this.state.clear
+
+        console.log(clear)
+
+        const currentValue = clear ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        this.setState({displayValue, clear:false})
+
+        if(n !==  '.'){
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({values})
+            console.log(values)
+        }
     }
 
     render() {
         return (
             <div className="calculator">
-                <Display value="" />
+                <Display value={this.state.displayValue} />
                 <Button label="AC" click =  {this.clearDisplay} triple/>
                 <Button label="/" click = {this.setOperation} operation/>
                 <Button label="7" click = {this.addDigit} />
@@ -42,7 +99,7 @@ export default class Calculator extends Component {
                 <Button label="3" click = {this.addDigit}/>
                 <Button label="+" click = {this.setOperation} operation/>
                 <Button label="0" click = {this.addDigit} double/>
-                <Button label="." click = {this.setOperation} />
+                <Button label="." click = {this.addDigit} />
                 <Button label="=" click = {this.setOperation} operation/>
             </div>
         )
